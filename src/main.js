df99b44d15f7d1f7010cd29eb6fecf4be8211a7f
Bruno Farias <brunofarias1@gmail.com>
2018-02-04 20:19:50 -0200

    base app initial commit

---
+++ src/main.js
@@ -0,0 +1,102 @@
+import Vue from 'vue'
+import {Team} from './team'
+import _ from 'lodash'
+
+require('bootstrap/dist/css/bootstrap.min.css')
+require('bootstrap')
+
+
+new Vue({
+  el: '#app',
+  data: {
+    order: {
+      keys: ['points', 'gp', 'ga'],
+      sort: ['desc', 'desc', 'asc']
+    },
+    filter: '',
+    columns: ['name', 'points', 'gp', 'ga', 'balance'],
+    teams: [
+      new Team('Palmeiras', require('./assets/palmeiras_60x60.png')),
+      new Team('Flamengo', require('./assets/flamengo_60x60.png')),
+      new Team('Atlético-MG', require('./assets/atletico_mg_60x60.png')),
+      new Team('Santos', require('./assets/santos_60x60.png')),
+      new Team('Botafogo', require('./assets/botafogo_60x60.png')),
+      new Team('Atlético-PR', require('./assets/atletico-pr_60x60.png')),
+      new Team('Corinthians', require('./assets/corinthians_60x60.png')),
+      new Team('Grêmio', require('./assets/gremio_60x60.png')),
+      new Team('Fluminense', require('./assets/fluminense_60x60.png')),
+      new Team('Ponte Preta', require('./assets/ponte_preta_60x60.png')),
+      new Team('Chapecoense', require('./assets/chapecoense_60x60.png')),
+      new Team('São Paulo', require('./assets/sao_paulo_60x60.png')),
+      new Team('Cruzeiro', require('./assets/cruzeiro_60x60.png')),
+      new Team('Sport', require('./assets/sport_60x60.png')),
+      new Team('Coritiba', require('./assets/coritiba_60x60.png')),
+      new Team('Internacional', require('./assets/internacional_60x60.png')),
+      new Team('Vitória', require('./assets/vitoria_60x60.png')),
+      new Team('Figueirense', require('./assets/figueirense_60x60.png')),
+      new Team('Santa Cruz', require('./assets/santa_cruz_60x60.png')),
+      new Team('América-MG', require('./assets/america_mg_60x60.png')),
+    ],
+    newGame: {
+      home: {
+        team: null,
+        goals: 0
+      },
+      visitor: {
+        team: null,
+        goals: 0
+      }
+    },
+    view: 'table'
+  },
+  methods: {
+    endGame() {
+      let adversary = this.newGame.visitor.team
+      let goals = +this.newGame.home.goals
+      let goalsAdversary = +this.newGame.visitor.goals
+      this.newGame.home.team.endGame(adversary, goals, goalsAdversary)
+      this.showView('table')
+
+    },
+    createNewGame() {
+      let indexHome = Math.floor(Math.random() * 20),
+        indexVisitor = Math.floor(Math.random() * 20)
+
+      if (indexHome === indexVisitor) {
+        indexVisitor = Math.floor(Math.random() * 20)
+      }
+
+      this.newGame.home.team = this.teams[indexHome]
+      this.newGame.home.goals = 0
+
+      this.newGame.visitor.team = this.teams[indexVisitor]
+      this.newGame.visitor.goals = 0
+
+      this.showView('newGame')
+    },
+    showView(view) {
+      this.view = view
+    },
+    sortBy(column) {
+      this.order.keys = column
+      this.order.sort = this.order.sort === 'desc' ? 'asc' : 'desc'
+    }
+  },
+  computed: {
+    teamsFiltered() {
+      let collection = _.orderBy(this.teams, this.order.keys, this.order.sort)
+      return _.filter(collection, item => {
+        return item.name.indexOf(this.filter) >= 0
+      })
+    },
+
+  },
+  filters: {
+    balance(team) {
+      return team.gp - team.ga
+    },
+    ucWords(value) {
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  }
+})
---
+++ src/team.js
@@ -0,0 +1,31 @@
+export class Team {
+
+  constructor(name, shield) {
+    this.name = name
+    this.shield = shield
+
+    this.point = 0
+    this.gp = 0
+    this.ga = 0
+  }
+
+  updateInfo(points, goalsPro, goalsAgainst) {
+    this.point += points
+    this.gp += goalsPro
+    this.ga += goalsAgainst
+  }
+
+  endGame(adversary, goals, goalsAdversary) {
+    if (goals === goalsAdversary) {
+      this.updateInfo(1, goals, goalsAdversary)
+      adversary.updateInfo(1, goals, goalsAdversary)
+    } else if (goals > goalsAdversary) {
+      this.updateInfo(3, goals, goalsAdversary)
+      adversary.updateInfo(0, goalsAdversary, goals)
+    } else {
+      this.updateInfo(0, goals, goalsAdversary)
+      adversary.updateInfo(3, goalsAdversary, goals)
+    }
+  }
+
+}
